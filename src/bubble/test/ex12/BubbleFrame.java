4113784dd5694daf09b2c8388ef304987bbb93c0
moonstal1506 <moonstal1506@gmail.com>
2022-03-13 01:54:19 +0900

    적군 생성

--- src/bubble/test/ex12/BubbleFrame.java
+++ src/bubble/test/ex12/BubbleFrame.java
@@ -17,6 +17,7 @@ public class BubbleFrame extends JFrame {
 	private BubbleFrame mContext = this;
 	private JLabel backgroundMap;
 	private Player player;
+	private Enemy enemy;
 
 	public BubbleFrame() {
 		initObject();
@@ -30,6 +31,8 @@ public class BubbleFrame extends JFrame {
 		setContentPane(backgroundMap);
 		player = new Player(mContext);
 		add(player);
+		enemy = new Enemy(mContext);
+		add(enemy);
 	}
 
 	private void initSetting() {
